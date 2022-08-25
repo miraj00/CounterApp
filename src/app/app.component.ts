d15fb5035b3565198c7a98633f9c4820d8d4d38f
miraj <miraj00@gmail.com>
2022-08-25 18:48:56 -0400

    increment & decrement counter added

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  counter: number = 0;
+
+  incrementCounter():void {
+    this.counter +=1;
+  }
+
+  decrementCounter():void {
+    this.counter -=1;
+  }
+}
